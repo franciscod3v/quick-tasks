08afaa99e8bb0bfcb6d67a145260437da1d23a9d
franciscod3v <franciscod3v@gmail.com>
2023-11-21 12:02:30 -0500

    Se esta trabajando en el componente TareaCard

--- src/components/ListaTareas.jsx
+++ src/components/ListaTareas.jsx
@@ -3,7 +3,7 @@ import { TareaCard } from './TareaCard'
 
 export const ListaTareas = () => {
     return (
-        <div className=''>
+        <div className='d-flex flex-column flex-md-row'>
             <div className='list-group'>
                 <h2>To Do</h2>
                 <TareaCard />
--- src/components/TareaCard.jsx
+++ src/components/TareaCard.jsx
@@ -1,15 +1,22 @@
+import './styles-components/tarea-card.css'
 
 export const TareaCard = () => {
   return (
-    <div className="list-group-item">
-        <h3>Titulo Tarea</h3>
-        <div>
-            <div>
-                <div>1</div>
-                <div>2</div>
-                <div>3</div>
+    <div className="tareaCard-container list-group-item container">
+        <div className='row'>
+          <h3 className="col-8 nombre-tarea">Soy una tarea</h3>
+          <div className='col-4 d-flex gap-1'>
+            <button className='btn btn-success'>Ok</button>
+            <button className='btn btn-danger'>X</button>
+          </div>
+        </div>
+        <div className='row'>
+            <div className='priority d-flex gap-1 col-8'>
+                <div className='priority-token low'>1</div>
+                <div className='priority-token medium'>2</div>
+                <div className='priority-token high'>3</div>
             </div>
-            <span>Prioridad</span>
+            <span className='col-4 category'>Categoria</span>
         </div>
     </div>
   )
