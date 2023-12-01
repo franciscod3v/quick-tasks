f995cc66e3fe395342f12e45297e22fdc9f4f0d8
Franciscod3v <franciscod3v@gmail.com>
2023-11-30 22:47:48 -0500

    Se empieza a crear hook useFetch

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,9 +1,41 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export const useFetch = (url) => {
 
-    const [data, setData] = useState(url)
-    
-    
+    const [data, setData] = useState(null)
+
+    const [loading, setLoading] = useState(null)
+
+    const [error, setError] = useState(null)
+
+    const [controller, setController] = useState(null)
+
+    useEffect(() => {
+        const abortController = new AbortController()
+        setController(abortController)
+
+        fetch(url, { signal: abortController.signal })
+            .then((response) => response.json)
+            .then(json => setData(json))
+            .catch(error => {
+                if (error.name === "AbortError") {
+                    console.log("Cancelled request")
+                } else {
+                    setError(error)
+                }
+            })
+            .finally(() => setLoading(false))
+
+        return () => abortController.abort()
+    }, [])
+
+    const handleCancelRequest = () => {
+        if (controller) {
+            controller.abort()
+            setError("Cancelled Request")
+        }
+    }
+
+    return { data, loading, error, handleCancelRequest }
 
 }
